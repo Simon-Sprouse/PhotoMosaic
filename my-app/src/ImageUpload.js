9d48e9ea7ac49eb9c7bb6441c2082ad0c1987648
Simon Sprouse <simonhsprouse@gmail.com>
2024-07-24 02:59:03 -0500

    added grid feature

--- my-app/src/ImageUpload.js
+++ my-app/src/ImageUpload.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function ImageUpload() { 
 
     const [image, setImage] = useState(null); // file
     const [preview, setPreview] = useState(''); // data url
+
+    const [chunks, setChunks] = useState([]);
+    const canvasRef = useRef(null);
 
 
     function handleImageUpload(event) { 
@@ -18,6 +21,77 @@ function ImageUpload() {
         }
     };
 
+    function divideImage(rows, cols) { 
+        if (!preview) return;
+
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+
+        const newImage = new Image();
+        newImage.src = preview;
+
+        newImage.onload = () => { 
+            const { width, height } = newImage;
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(newImage, 0, 0, width, height);
+
+            const chunkWidth = width / cols;
+            const chunkHeight = height / rows;
+
+            const chunksArray = [];
+
+            for (let row = 0; row < rows; row++) { 
+                for (let col = 0; col < cols; col++) { 
+                    const x = col * chunkWidth;
+                    const y = row * chunkHeight;
+
+                    const imageData = ctx.getImageData(x, y, chunkWidth, chunkHeight);
+
+                    chunksArray.push({
+                        row,
+                        col, 
+                        imageData,
+                        score: calculateChunkScore(imageData),
+                    })
+                }
+            }
+
+            setChunks(chunksArray);
+        }
+
+    }
+
+
+    function calculateChunkScore(imageData) { 
+
+        const data = imageData.data;
+        let totalScore = 0;
+
+        // loop through each pixel, four entries are RGBA
+        for (let i = 0; i < data.length; i += 4) { 
+            const score = 0.33 * data[i] + 0.33 * data[i + 1] + 0.33 * data[i + 1];
+            totalScore += score;
+        }
+
+        totalScore /= (data.length / 4);
+
+        return totalScore;
+    }
+
+
+
+
+
+    function handleButtonClick() { 
+        const rows = 4;
+        const cols = 4;
+        
+        divideImage(rows, cols);
+
+        console.log(chunks);
+    }
+
 
 
 
@@ -25,6 +99,7 @@ function ImageUpload() {
 
     return (
         <>
+            <canvas ref={canvasRef} style={{ display: 'none'}}/>
             <p>Upload an Image</p>
             <input 
                 type="file" 
@@ -35,8 +110,11 @@ function ImageUpload() {
                 <div>
                     <p>Image Preview:</p>
                     <img src={preview} />
+
                 </div>
             )}
+            <button onClick={handleButtonClick}>Generate Chunks</button>
+            
         </>
     )
 }
